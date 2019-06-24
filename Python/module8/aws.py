7c407f6550896ee60459a26963e6ccc71f8bf657
Petrov Mikhail <Mikhail.Petrov@t-systems.com>
2019-06-24 11:09:24 +0300

    Add part of Python module8

---
+++ Python/module8/aws.py
@@ -0,0 +1,31 @@
+from typing import List
+
+import boto3
+
+client = boto3.client('s3')
+s3 = boto3.resource('s3')
+
+bucket_to_copy = "bucket-id1231355"
+buckets_from_copy = ["bucket-id123123"]
+
+
+def copy_files(dest_bucket: str, src_buckets: List) -> None:
+    response = client.list_buckets()
+    if dest_bucket not in [bucket['Name'] for bucket in response['Buckets']]:
+        create_bucket(client, dest_bucket)
+
+    for bucket in src_buckets:
+        for key in s3.list_objects(Bucket=bucket)['Contents']:
+            files = key['Key']
+            copy_source = {'Bucket': bucket, 'Key': files}
+            s3.meta.client.copy(copy_source, dest_bucket, files)
+            print(files)
+
+
+def create_bucket(client: boto3.client, bucket_name: str):
+    client.create_bucket(Bucket=bucket_name)
+    print("New bucket {} was created".format(bucket_name))
+
+
+if __name__ == "__main__":
+    copy_files(dest_bucket=bucket_to_copy, src_buckets=buckets_from_copy)
